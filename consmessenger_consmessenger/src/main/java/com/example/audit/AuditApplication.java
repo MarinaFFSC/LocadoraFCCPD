0267ff50c04c71ed51c9b3b33aba0fa261cd271c
mjpc_cesar <mjpc_cesar@SCHOOL-013489>
2024-09-29 12:29:24 -0300

    Primeiro Commit

---
+++ consmessenger_consmessenger/src/main/java/com/example/audit/AuditApplication.java
@@ -0,0 +1,54 @@
+package com.example.audit;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class AuditApplication {
+
+    static final String topicExchangeName = "topic-exchange";
+    static final String queueName = "locadora.auditoria.queue";
+    static final String routingKey = "locadora.filmes.#"; 
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding() {
+        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(AuditReceiver auditReceiver) {
+        return new MessageListenerAdapter(auditReceiver, "auditMessage");
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditApplication.class, args);
+    }
+}
